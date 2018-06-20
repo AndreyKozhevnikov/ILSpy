9a04ed0ef44f6eb5a32bc515a71cb5dfef93e2d6
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-06-20 09:25:36 +0300

    add button to clear analyze

---
+++ ILSpy/TreeNodes/Analyzer/RemoveAnalyzeAllEntries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSharpCode.ILSpy.TreeNodes.Analyzer
+{
+	[ExportContextMenuEntry(Header = "RemoveAll", Icon = "images/Delete.png", Category = "Analyze", Order = 200)]
+	internal sealed class RemoveAnalyzeAllEntries : IContextMenuEntry
+	{
+		public bool IsVisible(TextViewContext context)
+		{
+			if (context.TreeView is AnalyzerTreeView && context.SelectedTreeNodes != null && context.SelectedTreeNodes.All(n => n.Parent.IsRoot))
+				return true;
+			return false;
+		}
+
+		public bool IsEnabled(TextViewContext context)
+		{
+			return true;
+		}
+
+		public void Execute(TextViewContext context)
+		{
+			context.TreeView.Root.Children.Clear();
+			//if (context.SelectedTreeNodes != null) {
+			//	foreach (var node in context.SelectedTreeNodes) {
+			//		node.Parent.Children.Remove(node);
+			//	}
+			//}
+		}
+	}
+}
